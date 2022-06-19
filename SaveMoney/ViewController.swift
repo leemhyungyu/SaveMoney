d0924c881b4c2a1600be363bda894610f7342606
leemhyungyu <lhglbkksj@naver.com>
2022-06-19 19:59:18 +0900

    Initial Commit

---
+++ SaveMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaveMoney
+//
+//  Created by 임현규 on 2022/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
