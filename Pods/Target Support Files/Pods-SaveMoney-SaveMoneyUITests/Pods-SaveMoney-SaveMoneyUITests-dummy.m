7b1f2e84eb81029ace8859db723647d91063bf24
leemhyungyu <lhglbkksj@naver.com>
2022-08-30 23:41:53 +0900

    Mod: Pods 폴더 삭제

--- Pods/Target Support Files/Pods-SaveMoney-SaveMoneyUITests/Pods-SaveMoney-SaveMoneyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SaveMoney_SaveMoneyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SaveMoney_SaveMoneyUITests
-@end
--- Pods/Target Support Files/Pods-SaveMoneyTests/Pods-SaveMoneyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SaveMoneyTests : NSObject
-@end
-@implementation PodsDummy_Pods_SaveMoneyTests
-@end
